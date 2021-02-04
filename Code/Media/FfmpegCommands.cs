ef76ea81a03b8fdd384d9a458549cea4c253426f
N00MKRAD <me@nmkd.de>
2021-02-04 23:29:46 +0100

    Split ffmpeg alpha commands into their own class

--- Code/Media/FfmpegCommands.cs
+++ Code/Media/FfmpegCommands.cs
@@ -31,40 +31,6 @@ namespace Flowframes
             await RunFfmpeg(args, concatFile.GetParentDir(), LogMode.Hidden, TaskType.Merge);
         }
 
-        public static async Task ExtractAlphaDir (string rgbDir, string alphaDir)
-        {
-            Directory.CreateDirectory(alphaDir);
-            foreach (FileInfo file in IOUtils.GetFileInfosSorted(rgbDir))
-            {
-                string args = $"-i {file.FullName.Wrap()} -vf format=yuva444p16le,alphaextract,format=yuv420p {Path.Combine(alphaDir, file.Name).Wrap()}";
-                await RunFfmpeg(args, LogMode.Hidden);
-            }
-        }
-
-        public static async Task RemoveAlpha (string inputDir, string outputDir, string fillColor = "black")
-        {
-            Directory.CreateDirectory(outputDir);
-            foreach (FileInfo file in IOUtils.GetFileInfosSorted(inputDir))
-            {
-                string outFilename = Path.Combine(outputDir, "_" + file.Name);
-                Size res = IOUtils.GetImage(file.FullName).Size;
-                string args = $" -f lavfi -i color={fillColor}:s={res.Width}x{res.Height} -i {file.FullName.Wrap()} " +
-                    $"-filter_complex overlay=0:0:shortest=1 -pix_fmt rgb24 {outFilename.Wrap()}";
-                await RunFfmpeg(args, LogMode.Hidden);
-                file.Delete();
-                File.Move(outFilename, file.FullName);
-            }
-        }
-
-        public static async Task MergeAlphaIntoRgb (string rgbDir, int rgbPad, string alphaDir, int aPad, bool deleteAlphaDir)
-        {
-            string filter = "-filter_complex [0:v:0][1:v:0]alphamerge[out] -map [out]";
-            string args = $"-i \"{rgbDir}/%{rgbPad}d.png\" -i \"{alphaDir}/%{aPad}d.png\" {filter} \"{rgbDir}/%{rgbPad}d.png\"";
-            await RunFfmpeg(args, LogMode.Hidden);
-            if (deleteAlphaDir)
-                IOUtils.TryDeleteIfExists(alphaDir);
-        }
-
         public static async Task LoopVideo(string inputFile, int times, bool delSrc = false)
         {
             string pathNoExt = Path.ChangeExtension(inputFile, null);
