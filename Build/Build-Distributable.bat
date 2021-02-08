e990ec05054995dbccc00ba5bb8a225385a05a8d
Dankrushen <henryclose@gmail.com>
2021-02-08 15:48:44 -0500

    Replace original code with .NET 5

--- Build/Build-Distributable.bat
+++ Build/Build-Distributable.bat
@@ -10,41 +10,48 @@ echo.
 set "ver=16"
 set /p ver="Enter the version number: "
 
-cd ..\Code\bin\x64\Release
+cd ..
+
+dotnet publish -c Release -o "Release/Build" "Code5"
+
+cd Release
 
 rmdir /s/q FlowframesApp%ver%
 mkdir "FlowframesApp%ver%"
 mkdir "FlowframesApp%ver%/FlowframesData"
 mkdir "FlowframesApp%ver%/FlowframesData/pkgs"
 
-rem xcopy "../../../../pkgs" "FlowframesApp%ver%/FlowframesData\pkgs\" /E
-xcopy "../../../../pkgs/av" "FlowframesApp%ver%/FlowframesData\pkgs\av" /E /I
-xcopy "../../../../pkgs/dain-ncnn" "FlowframesApp%ver%/FlowframesData\pkgs\dain-ncnn" /E /I
-xcopy "../../../../pkgs/licenses" "FlowframesApp%ver%/FlowframesData\pkgs\licenses" /E /I
-xcopy "../../../../pkgs/rife-cuda" "FlowframesApp%ver%/FlowframesData\pkgs\rife-cuda" /E /I
-xcopy "../../../../pkgs/rife-ncnn" "FlowframesApp%ver%/FlowframesData\pkgs\rife-ncnn" /E /I
+rem xcopy "../pkgs" "FlowframesApp%ver%/FlowframesData\pkgs\" /E
+xcopy "../pkgs/av" "FlowframesApp%ver%/FlowframesData\pkgs\av" /E /I
+xcopy "../pkgs/dain-ncnn" "FlowframesApp%ver%/FlowframesData\pkgs\dain-ncnn" /E /I
+xcopy "../pkgs/licenses" "FlowframesApp%ver%/FlowframesData\pkgs\licenses" /E /I
+xcopy "../pkgs/rife-cuda" "FlowframesApp%ver%/FlowframesData\pkgs\rife-cuda" /E /I
+xcopy "../pkgs/rife-ncnn" "FlowframesApp%ver%/FlowframesData\pkgs\rife-ncnn" /E /I
 
 echo %ver% >> "FlowframesApp%ver%/FlowframesData/ver.ini"
 
-xcopy Flowframes.exe "FlowframesApp%ver%"
+xcopy /E "Build" "FlowframesApp%ver%"
 
-cd ../../../../Build
+cd ..\Build5
 
-rmdir /s/q ..\Code\bin\x64\Release\FlowframesApp%ver%\FlowframesData\logs
-del ..\Code\bin\x64\Release\FlowframesApp%ver%\FlowframesData\config.ini
-
-
-7za.exe a FF-%ver%-Slim.7z -m0=flzma2 -mx5 "..\Code\bin\x64\Release\FlowframesApp%ver%"
-
-xcopy "../pkgs/py-tu" "..\Code\bin\x64\Release\FlowframesApp%ver%\FlowframesData\pkgs\py-tu" /E /I
-7za.exe a FF-%ver%-Full.7z -m0=flzma2 -mx7 "..\Code\bin\x64\Release\FlowframesApp%ver%"
-
-rmdir /s/q ..\Code\bin\x64\Release\FlowframesApp%ver%\FlowframesData\pkgs\py-tu
-xcopy "../pkgs/py-amp" "..\Code\bin\x64\Release\FlowframesApp%ver%\FlowframesData\pkgs\py-amp" /E /I
-7za.exe a FF-%ver%-Full-RTX3000.7z -m0=flzma2 -mx7 "..\Code\bin\x64\Release\FlowframesApp%ver%"
+rmdir /s/q ..\Release\FlowframesApp%ver%\FlowframesData\logs
+del ..\Release\FlowframesApp%ver%\FlowframesData\config.ini
 
 
-rmdir /s/q ..\Code\bin\x64\Release\FlowframesApp%ver%
+del FF-%ver%-Slim.7z
+7za.exe a FF-%ver%-Slim.7z -m0=flzma2 -mx5 "..\Release\FlowframesApp%ver%"
+
+xcopy "../pkgs/py-tu" "..\Release\FlowframesApp%ver%\FlowframesData\pkgs\py-tu" /E /I
+del FF-%ver%-Full.7z
+7za.exe a FF-%ver%-Full.7z -m0=flzma2 -mx7 "..\Release\FlowframesApp%ver%"
+
+rmdir /s/q ..\Release\FlowframesApp%ver%\FlowframesData\pkgs\py-tu
+xcopy "../pkgs/py-amp" "..\Release\FlowframesApp%ver%\FlowframesData\pkgs\py-amp" /E /I
+del FF-%ver%-Full-RTX3000.7z
+7za.exe a FF-%ver%-Full-RTX3000.7z -m0=flzma2 -mx7 "..\Release\FlowframesApp%ver%"
+
+
+rmdir /s/q ..\Release\FlowframesApp%ver%
 
 
 rem pause
