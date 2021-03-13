0f4ec91fafcd9ebc28ee441cf2b88cf2429b3e4f
N00MKRAD <login@nmkd.de>
2021-03-13 14:32:29 +0100

    Get changelog and patron list from repo

--- Code/UI/GetWebInfo.cs
+++ Code/UI/GetWebInfo.cs
@@ -12,18 +12,10 @@ namespace Flowframes.UI
     {
         public static async Task LoadNews (Label newsLabel)
         {
-            string url = $"http://dl.nmkd.de/flowframes/changelog.txt";
+            string url = $"https://raw.githubusercontent.com/n00mkrad/flowframes/main/changelog.txt";
             var client = new WebClient();
             var str = await client.DownloadStringTaskAsync(new Uri(url));
             newsLabel.Text = str;
-        }
-
-        public static async Task LoadPatronList(Label patronsLabel)
-        {
-            string url = $"http://dl.nmkd.de/flowframes/patreon.txt";
-            var client = new WebClient();
-            var str = await client.DownloadStringTaskAsync(new Uri(url));
-            patronsLabel.Text = str;
         }
 
         public static async Task LoadPatronListCsv(Label patronsLabel)
