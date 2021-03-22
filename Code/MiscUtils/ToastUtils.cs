3891c4dea23925dd9c290b923632484aca7c4c7e
Richard <mail@richard.yt>
2021-03-22 18:14:23 +0100

    add ToastUtils

---
+++ Code/MiscUtils/ToastUtils.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace Flowframes.MiscUtils
+{
+    public class ToastUtils
+    {
+        public static void ShowToast(string title, string content)
+        {
+            new ToastContentBuilder()
+                .AddText(title)
+                .AddText(content)
+                .Show();
+        }
+    }
+}
