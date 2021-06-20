bbeb6913ff630d2903e83fa52d33580e68e0667c
N00MKRAD <login@nmkd.de>
2021-06-20 19:33:23 +0200

    Added support for custom models
    Dir name must end with "_custom", must be only A-Z, 0-9 and underscores

--- Code/Main/AiModels.cs
+++ Code/Main/AiModels.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Flowframes.Main
@@ -16,7 +17,29 @@ namespace Flowframes.Main
             string pkgPath = Path.Combine(Paths.GetPkgPath(), ai.pkgDir);
             string modelsFile = Path.Combine(pkgPath, "models.json");
             ModelCollection modelCollection = new ModelCollection(ai, modelsFile);
+
+            foreach (string customModel in GetCustomModels(ai))
+            {
+                string name = customModel.Remove("_alpha").Remove("_custom");
+                bool alpha = customModel.Contains("_alpha");
+                modelCollection.models.Add(new ModelCollection.ModelInfo(ai, name, "Custom Model", customModel, alpha, false));
+            }
+
             return modelCollection;
+        }
+
+        public static List<string> GetCustomModels(AI ai)
+        {
+            string pkgPath = Path.Combine(Paths.GetPkgPath(), ai.pkgDir);
+            List<string> custModels = new List<string>();
+
+            foreach (DirectoryInfo dir in new DirectoryInfo(pkgPath).GetDirectories())
+            {
+                if (dir.Name.EndsWith("_custom") && Regex.IsMatch(dir.Name, @"^[a-zA-Z0-9_]+$"))
+                    custModels.Add(dir.Name);
+            }
+
+            return custModels;
         }
 
         public static ModelCollection.ModelInfo GetModelByName(AI ai, string modelName)
