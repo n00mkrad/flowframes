c9ccbcc9c96ee7e829ebf2aa7034131a65b64b55
n00mkrad <login@nmkd.de>
2022-08-04 15:16:32 +0200

    Replace deprecated -vsync with -fps_mode (ffmpeg)

--- Code/Media/FfmpegEncode.cs
+++ Code/Media/FfmpegEncode.cs
@@ -40,7 +40,7 @@ namespace Flowframes.Media
                 string pre = i == 0 ? "" : $" && ffmpeg {AvProcess.GetFfmpegDefaultArgs()}";
                 string post = (i == 0 && encArgs.Length > 1) ? $"-f null -" : outPath.Wrap();
                 string fs = (!isChunk && outMode == Interpolate.OutMode.VidMp4) ? $"-movflags +faststart" : "";
-                args += $"{pre} -vsync 0 -r {fps} {inArg} {encArgs[i]} {GetFfmpegExportArgs(resampleFps, extraData)} -threads {Config.GetInt(Config.Key.ffEncThreads)} {fs} {post} ";
+                args += $"{pre} -fps_mode 0 -r {fps} {inArg} {encArgs[i]} {GetFfmpegExportArgs(resampleFps, extraData)} -threads {Config.GetInt(Config.Key.ffEncThreads)} {fs} {post} ";
             }
 
             await RunFfmpeg(args, framesFile.GetParentDir(), logMode, !isChunk);
@@ -91,7 +91,7 @@ namespace Flowframes.Media
             string vf = (resampleFps.GetFloat() < 0.1f) ? "" : $"-vf fps=fps={resampleFps}";
             string compression = format == "png" ? pngCompr : $"-q:v {lossyQ}";
             string codec = format == "webp" ? "-c:v libwebp" : ""; // Specify libwebp to avoid putting all frames into single animated WEBP
-            string args = $"-vsync 0 -r {rate} {inArg} {codec} {compression} {sn} {vf} \"{outDir}/%{Padding.interpFrames}d.{format}\"";
+            string args = $"-fps_mode 0 -r {rate} {inArg} {codec} {compression} {sn} {vf} \"{outDir}/%{Padding.interpFrames}d.{format}\"";
             await RunFfmpeg(args, framesFile.GetParentDir(), logMode, "error", true);
             IoUtils.TryDeleteIfExists(linksDir);
         }
