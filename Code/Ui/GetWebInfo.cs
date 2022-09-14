7847d58284760e26da6b3f50691db7d69aa751c7
n00mkrad <login@nmkd.de>
2022-09-14 10:27:48 +0200

    cleanup

--- Code/Ui/GetWebInfo.cs
+++ Code/Ui/GetWebInfo.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.NetworkInformation;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.VisualBasic.Logging;
 
 namespace Flowframes.Ui
 {
