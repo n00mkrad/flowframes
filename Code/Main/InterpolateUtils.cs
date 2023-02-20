c112f0516df1012331069654c5f04d20b24691f7
n00mkrad <login@nmkd.de>
2023-02-20 16:00:27 +0100

    Update InterpolateUtils.cs

--- Code/Main/InterpolateUtils.cs
+++ Code/Main/InterpolateUtils.cs
@@ -366,7 +366,7 @@ namespace Flowframes.Main
 
         public static Fraction AskForFramerate (string mediaName, bool isImageSequence = true)
         {
-            string text = $"Please enter a frame rate to use for{(isImageSequence ? " the image sequence" : "")} '{mediaName.Trunc(80)}'.";
+            string text = $"Please enter an input frame rate to use for{(isImageSequence ? " the image sequence" : "")} '{mediaName.Trunc(80)}'.";
             PromptForm form = new PromptForm("Enter Frame Rate", text, "15");
             form.ShowDialog();
             return new Fraction(form.EnteredText);
