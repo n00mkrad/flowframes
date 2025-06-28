0e6bc5830729c48718ca01e8ca3f6372b8f860b4
n00mkrad <61149547+n00mkrad@users.noreply.github.com>
2025-06-28 22:34:47 +0200

    PseudoHash directory support

--- CodeLegacy/Data/PseudoHash.cs
+++ CodeLegacy/Data/PseudoHash.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Flowframes.IO;
+using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Flowframes.Data
@@ -8,6 +10,16 @@ namespace Flowframes.Data
     {
         public static string GetHash(string path, bool b64 = true)
         {
+            bool isDir = Directory.Exists(path);
+
+            if (isDir)
+            {
+                var dir = new DirectoryInfo(path);
+                var files = IoUtils.GetFileInfosSorted(path);
+                string dirHash = $"{dir.Name}{files.Sum(f => f.Length)}{dir.LastWriteTime.ToString("yyyyMMddHHmmss")}";
+                return b64 ? Convert.ToBase64String(Encoding.UTF8.GetBytes(dirHash)).TrimEnd('=') : dirHash;
+            }
+
             var file = new FileInfo(path);
             string hash = $"{file.Name}{file.Length}{file.LastWriteTime.ToString("yyyyMMddHHmmss")}";
             return b64 ? Convert.ToBase64String(Encoding.UTF8.GetBytes(hash)).TrimEnd('=') : hash;
